963b154af3e76e0f92864e7d0e340f151460b2c9
nikhil-keer <nikhilkeer03@gmail.com>
2026-01-28 01:19:50 -0500

    Add files via upload

---
+++ Tensorflow_Test.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+"""
+
+tf.debugging.set_log_device_placement(True)
+
+# Place tensors on the CPU
+with tf.device('/CPU:0'):
+  a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+  b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+
+# Run on the GPU
+c = tf.matmul(a, b)
+print(c)
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only use the first GPU
+  try:
+    tf.config.set_visible_devices(gpus[0], 'GPU')
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+  except RuntimeError as e:
+    # Visible devices must be set before GPUs have been initialized
+    print(e)
+
+
+
+tf.debugging.set_log_device_placement(True)
+
+try:
+  # Specify an invalid GPU device
+  with tf.device('/device:GPU:2'):
+    a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+    c = tf.matmul(a, b)
+except RuntimeError as e:
+  print(e)  
+
+
+tf.config.set_soft_device_placement(True)
+tf.debugging.set_log_device_placement(True)
+
+# Creates some tensors
+a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+c = tf.matmul(a, b)
+
+print(c) """
+
+tf.debugging.set_log_device_placement(True)
+
+# Place tensors on the CPU
+with tf.device('/CPU:0'):
+  a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+  b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+
+# Run on the GPU
+c = tf.matmul(a, b)
+print(c)
+
+
+if tf.config.list_physical_devices('GPU'):
+  print("TensorFlow **IS** using the GPU")
+else:
+  print("TensorFlow **IS NOT** using the GPU")
+
+
+
