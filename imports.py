d2da6bd7b21175e4c8d989546639653dc5861f9a
Bryson-cm <brysoncm@icloud.com>
2026-06-12 12:10:45 -0400

    Created an import file to determine which packages I need to install

---
+++ imports.py
@@ -0,0 +1,26 @@
+from math import e
+import numpy as np
+import matplotlib.colors as col
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.cm as cm
+import matplotlib.ticker as ticker
+from mpl_toolkits.mplot3d import Axes3D
+import pdb
+from scipy.optimize import curve_fit
+from copy import deepcopy
+import sys
+import math
+import numpy as np
+import h5py as h5
+import importlib
+import pdb
+import time
+import pickle
+import multiprocessing as mp
+from DebugObjectModule import DebugObject
+from numba import jit, cuda    # Imports to run on GPU
+from tqdm import tqdm
+
+# Include file imports
+import eProbe
