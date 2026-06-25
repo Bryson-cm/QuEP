9ef03d39ba2664e46fddfabc827b6cf0a9b520aa
Bryson-cm <brysoncm@icloud.com>
2026-06-25 16:28:46 -0400

    added an extra argument to vline and fixed code so it is able to run with new input files.

--- include/shapes/vline.py
+++ include/shapes/vline.py
@@ -5,14 +5,14 @@ import pdb
 
 # Initalizes probe as vertical line of electrons with length 2*s1
 
-def initProbe(x_c,y_c,xi_c,t0,s1,s2,s3,density):
+def initProbe(x_c,y_c,xi_c,t0,s1,s2,xdensity,ydensity,xidensity,resolution,):
 
     x_0, y_0, xi_0, z_0 = [],[],[],[]
 
-    step = 2*s1 / density
+    step = 2*s1 / ydensity
     yin = y_c - s1
 
-    for i in range(0,density):
+    for i in range(0,ydensity):
         x_0.append(x_c)
         xi_0.append(xi_c)
         z_0.append(xi_c + t0)
@@ -21,3 +21,6 @@ def initProbe(x_c,y_c,xi_c,t0,s1,s2,s3,density):
         yin += step
 
     return x_0, y_0, xi_0, z_0
+
+
+
