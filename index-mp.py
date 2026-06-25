2045857836b6c7eca1bee63a988d94cb6ba61741
Bryson-cm <brysoncm@icloud.com>
2026-06-25 16:28:12 -0400

    Started cleaning up index-mp. Began with a cleanup of quick evolution. and findfocaly. Added additional arguments to both. fig_name. added t0 to quick evolution

--- index-mp.py
+++ index-mp.py
@@ -7,62 +7,65 @@ import sys
 import time
 import importlib
 import numpy as np
-import include.showQuickEvolution as showEvol_Q
-import include.showFullEvolution as showEvol_F
-import include.makeFullAnimation as makeFullAni
 import include.viewProbe as viewProbe
-import include.writeFullEvolData as writeHist
-import include.weighting_masks_function_rprism as weightmaskFunc
-import include.plotWeights as plotWeights
-import include.findFocalY as findFocalY
-import include.plot2DTracks as plot2D
-import include.plot3DTracks as plot3D
-import include.findWaist as findWaist
+#import include.weighting_masks_function_rprism as weightmaskFunc
+
 import multiprocessing as mp
-#import include.movieWriter as movieWriter
+
 import tqdm
 import pickle
 from DebugObjectModule import DebugObject
 from random import randint
 
 
-
+##############################################################################################################
+#Notes
 # Be sure to change .npz file name location from main.py output!
 # Put .npz file in /data directory
+##############################################################################################################
+#Import the trial variables from the second argument on the command line. This will provide the tests adn trials that you want to complete. 
+#Import file is called index-mp-PLOTS.py 
+input_fname_2 = str(sys.argv[2])
+init2 = importlib.import_module(input_fname_2)
 
-# Weighting Options (Only applicable for showFullEvolution and makeFullAnimation plot):
-useWeights_x = False                 # NOT CURRENTLY IN USE - LEAVE FALSE - Use weights in x-direction
-useWeights_y = False                  # Use gaussian weights in y-direction
-useWeights_xi = False                 # Use gaussian weights in xi-direction
+#import output figure name
+fig_name = init2.fig_name
 
-skipWeightingCalc = False            # Skip weighting calculation and use imported pre-calculated weights
-saveWeights = True                 # Save weights to .npz file (Remember to move to ./data directory!)
+# Weighting Options
+useWeights_x = init2.useWeights_x          # Use weights in x-direction
+useWeights_y = init2.useWeights_y          # Use Gaussian weights in y-direction
+useWeights_xi = init2.useWeights_xi        # Use Gaussian weights in xi-direction
 
-# Masking Options:
-useMasks_xi = False                 # Use masks in xi-direction (Vertical; done during weighting)
-useMasks_y = False                  # Use masks in y-direction (Horizontal; done during weighting)
-useMasks_x = False                  # NOT CURRENTLY IN USE - LEAVE FALSE - Use masks in x-direction (transverse; done during weighting)
+skipWeightingCalc = init2.skipWeightingCalc    # Skip weighting calculation and use imported weights
+saveWeights = init2.saveWeights                # Save calculated weights to .npz file
 
-# Plotting Scripts
-showQuickEvolution = True           # View evolution of probe after leaving plasma at inputted x_s in scatter plots # Use for low density probes
-showFullEvolution = False             # View full evolution of probe at hardcoded locations in colored histograms # Use for high density probes
-makeFullAnimation = False
-writeHistData = False
+# Masking Options
+useMasks_xi = init2.useMasks_xi            # Use masks in xi-direction
+useMasks_y = init2.useMasks_y              # Use masks in y-direction
+useMasks_x = init2.useMasks_x              # Use masks in x-direction
 
-# Gaussian Weighting Testing
-plotWeightsx = False                  # Plot w vs xi (ONLY for single line of particles in x-dir)
-plotWeightsy = False                  # Plot w vs y (ONLY for single line of particles in y-dir)
-plotWeightsxi = False                  # Plot w vs y (ONLY for single line of particles in xi-dir)
-plotWeightsxiy = False                 # Plots initial particle density map
-plotWeights3D = False                  # Plots y, xi, and 2D cross section
+# Plotting Options
+showQuickEvolution = init2.showQuickEvolution      # Scatter plot of probe after leaving plasma
+showFullEvolution = init2.showFullEvolution        # Colored histogram evolution plots
+makeFullAnimation = init2.makeFullAnimation        # Generate full animation
+writeHistData = init2.writeHistData                # Save histogram data
 
-# DEBUG PLOTTING
-plot2DTracks = False                 # View 2D projections of trajectories (SET ALL OTHERS TO FALSE & ONLY USE FOR SINGLE PARTICLE)
-findFocal = True
-plot3DTracks = False
-findW = False
+# Gaussian Weighting Test Plots
+plotWeightsx = init2.plotWeightsx          # Plot weights vs. x
+plotWeightsy = init2.plotWeightsy          # Plot weights vs. y
+plotWeightsxi = init2.plotWeightsxi        # Plot weights vs. xi
+plotWeightsxiy = init2.plotWeightsxiy      # Plot initial particle density map
+plotWeights3D = init2.plotWeights3D        # Plot 3D weighting distribution
+
+# Debug Plotting Options
+plot2DTracks = init2.plot2DTracks          # Plot 2D particle trajectories
+findFocal = init2.findFocal                # Perform focal point analysis
+plot3DTracks = init2.plot3DTracks          # Plot 3D particle trajectories
+findW = init2.findW                        # Calculate beam waist
 
 
+##############################################################################################################
+#Begin Program once all trial data has been input
 
 if __name__ == '__main__':
     # Start of main()
@@ -80,7 +83,7 @@ if __name__ == '__main__':
         
         # Get inital conditions of probe again
         input_fname_1 = str(sys.argv[1])
-        print("Using initial conditions from ", input_fname_1)
+        #print("Using initial conditions from ", input_fname_1)
         init = importlib.import_module(input_fname_1)
        # sim.configure(init)
 
@@ -104,11 +107,11 @@ if __name__ == '__main__':
         s1 = init.s1
         s2 = init.s2
 
-        if len(sys.argv) == 3:
+        if len(sys.argv) == 4:
             # Get initial conditions of beam
-            input_fname_2 = str(sys.argv[2])
-            print("Using beam conditions from ", input_fname_2)
-            beaminit = importlib.import_module(input_fname_2)
+            input_fname_3 = str(sys.argv[3])
+            print("Using beam conditions from ", input_fname_3)
+            beaminit = importlib.import_module(input_fname_3)
             beamx_c = beaminit.beamx_c
             beamy_c = beaminit.beamy_c
             beamxi_c = beaminit.beamxi_c
@@ -187,17 +190,36 @@ if __name__ == '__main__':
                # np.savez(weights_fname, w=w)
               #  print(f"\nWeights saved to {weights_fname + '.npz'}\n") #Saves weights for reuse
 
+
+        ##############################################################################################################
         # Plot data points
-        print("Plotting...")
+        print("Plotting...\n")
+
+
+
+
+
+        ##############################################################################################################
+        #Plot Quick Evolution. The Evolution figure will save as a png file. The Quick Evolution file is for the personal computor to examine the size and evolution of
+        #low density beams. 
         if (showQuickEvolution):
-            showEvol_Q.plot(x_f, y_f, xi_f, z_f, px_f, py_f, pz_f, sim_name, shape_name, x_s, noObj, iter) # Note: does not use weights
-        
+            import include.showQuickEvolution as showEvol_Q
+            showEvol_Q.plot(x_f, y_f, xi_f, z_f, px_f, py_f, pz_f, t0, sim_name, shape_name, x_s, noObj, iter, fig_name) # Note: does not use weights
+            print("Moving to next module\n")
+        ##############################################################################################################
+
         if (showFullEvolution):
+            import include.showFullEvolution as showEvol_F
             showEvol_F.plot(x_f, y_f, xi_f, z_f, px_f, py_f, pz_f, w, sim_name, shape_name, noObj, iter)
-        
+            print("Moving to next module\n")
+        ##############################################################################################################
+ 
         if (makeFullAnimation):
+            import include.makeFullAnimation as makeFullAni
+            import include.movieWriter as movieWriter
             #Prepare plotting variables
             plasma_bnds, slices, xs_norm, yslice, zslice, bin_edges_z, bin_edges_y, cmap, cmin, vmin_, vmax_, zmin, zmax, ymin, ymax, fps, new_path, screen_dists = makeFullAni.prepare(sim_name, shape_name, noObj, rand)
+            print("Moving to next module\n")
             
             # Multiprocessing: propagate to each screen and create frame
             start_time_pfc = time.time()
@@ -216,44 +238,63 @@ if __name__ == '__main__':
             print("MP PFC - END TIME: ", curr_time_pfc_end)
             print("MP PFC - DURATION: ", (time.time() - start_time_pfc)/60, " min\n")
             
-            # Stitch frames into movie
-            #movieWriter.generatemovie(fps,new_path)
+            #Stitch frames into movie
+            movieWriter.generatemovie(fps,new_path)
+
+            print("Moving to next module\n")
+        ##############################################################################################################
             
         if (writeHistData):
+            import include.writeFullEvolData as writeHist
             writeHist.plot(x_f, y_f, xi_f, z_f, px_f, py_f, pz_f, sim_name, shape_name, noObj, iter)
-        
+        ##############################################################################################################
+
+
+
+        ##############################################################################################################
+        #If any of the plot weight functions are being used import plotWeights
+        if plotWeightsx or plotWeightsxi or plotWeightsy or plotWeightsxiy or plotWeights3D:
+            import include.plotWeights as plotWeights 
         if (plotWeightsy):
             plotWeights.ploty(w_y, x_0, y_0, xi_0, z_0, s1, s2, yden, xiden, beamy_c, sigma_y)
-        
         if (plotWeightsx):
             #plotWeights.plotx(w, x_0, y_0, xi_0, z_0, s1, s2, beamx_c,beamy_c,beamxi_c,sigma_x,sigma_y,sigma_xi)
             raise NotImplementedError("This functionality is not currently implemented")
             # This function could be easily added when needed
-
         if (plotWeightsxi):
             plotWeights.plotxi(w_xi, x_0, y_0, xi_0, z_0, s1, s2, yden, xiden, beamxi_c, sigma_xi)
-
         if (plotWeightsxiy):
             # Plots initial map of particle density
             plotWeights.plotweightsxiy(y_0,xi_0, w, rand)
-        
         if (plotWeights3D):
             plotWeights.plotcross(w_export1, x_0, y_0, xi_0, z_0, s1, s2, yden, xiden, beamxi_c, sigma_xi)
             #plotWeights.ploty(w_y, x_0, y_0, xi_0, z_0, s1, s2, yden, xiden)
             #plotWeights.plotxi(w_xi, x_0, y_0, xi_0, z_0, s1, s2, yden, xiden)
-
-        if (findFocal):
-            findFocalY.calculate(x_0, y_0, xi_0, z_0, x_dat, y_dat, z_dat, xi_dat, px_f, py_f, pz_f, sim_name, shape_name, x_s, s1, s2,px_0,py_0,pz_0)
-        
+        ##############################################################################################################
+        #If findfocal module is set to true, import find focal code, and run the findFocalY program. If you are using a single particle (debugmode == True) 
+        # it will run the debug mode data. Otherwise it will run the vlines data. for this code to work,
+        # either the debugmode must be set to true, or the shape of the probe must be a vline. 
+        if findFocal and (debugmode or shape_name == "vline"):
+            import include.findFocalY as findFocalY
+            if debugmode == True:
+                findFocalY.calculate(x_0, y_0, xi_0, z_0, x_dat, y_dat, z_dat, xi_dat, px_f, py_f, pz_f, sim_name, shape_name, x_s, s1, s2,px_0,py_0,pz_0)
+            else:
+                findFocalY.calculate(x_0, y_0, xi_0, z_0, x_f, y_f, z_f, xi_f, px_f, py_f, pz_f, sim_name, shape_name, x_s, s1, s2,px_0,py_0,pz_0)
+            print("Moving to next module\n")
+        ##############################################################################################################
         if (plot2DTracks):
+            import include.plot2DTracks as plot2D
             print("Plotting 2D Tracks...")
             plot2D.plot(x_dat, y_dat, z_dat, xi_dat, Fx_dat, Fy_dat, Fz_dat, px_dat, py_dat, pz_dat, sim_name, shape_name, s1, s2, noObj, fname)
         
         if (plot3DTracks):
+            import include.plot3DTracks as plot3D
             plot3D.plot(x_dat,y_dat,z_dat,xi_dat,sim_name,shape_name,s1,s2,noObj)
         
         if (findW):
+            import include.findWaist as findWaist
             findWaist.calculate(x_0,y_0,xi_0,z_0,x_dat,y_dat,z_dat,xi_dat,px_f,py_f,pz_f,sim_name,shape_name,x_s,s1,s2)
+        ##############################################################################################################
 
         # End timing index file runtime
         tf = time.localtime()
@@ -261,7 +302,7 @@ if __name__ == '__main__':
         print("index.py - END TIME: ", curr_time_f)
         print("index.py - DURATION: ", (time.time() - start_time)/60, " min\n")
     else:
-        print("Improper number of arguments. Expected 'python3 index.py <fname> *<fname>'")
+        print("Improper number of arguments. Expected 'python3 index-mp.py <fname> input.index-mp-PLOTS'")
         exit()
     
     pool.close()
